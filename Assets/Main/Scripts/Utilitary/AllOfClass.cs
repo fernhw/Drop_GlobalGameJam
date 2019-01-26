096c91485d7353ecc2ec636212f4c8e21274e8c0
fernhw <contact@fernhw.com>
2019-01-26 12:54:59 -0500

    all base files

---
+++ Assets/Main/Scripts/Utilitary/AllOfClass.cs
@@ -0,0 +1,35 @@
+﻿
+/* Copyright (C) Fernando Holguin Weber, and Studio Libeccio - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ * Written by Fernando Holguin <contact@fernhw.com>, January 2017
+ * 
+ * UNDER NO CIRCUMSTANCES IS Fernando Holguin Weber, OR STUDIO LIBECCIO, ITS PROGRAM DEVELOPERS OR SUPPLIERS LIABLE FOR ANY OF THE FOLLOWING, EVEN IF INFORMED OF THEIR POSSIBILITY: 
+ * LOSS OF, OR DAMAGE TO, DATA;
+ * DIRECT, SPECIAL, INCIDENTAL, OR INDIRECT DAMAGES, OR FOR ANY ECONOMIC CONSEQUENTIAL DAMAGES; OR
+ * LOST PROFITS, BUSINESS, REVENUE, GOODWILL, OR ANTICIPATED SAVINGS.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AllOfClass  {
+
+	public static List <SelectedClass> PickAllOf <SelectedClass> () where SelectedClass : class
+    {
+        MonoBehaviour[] monoBehaviours = UnityEngine.Object.FindObjectsOfType<MonoBehaviour>();
+        List<SelectedClass> list = new List<SelectedClass>();
+
+        foreach (MonoBehaviour behaviour in monoBehaviours)
+        {
+            SelectedClass component = behaviour.GetComponent(typeof(SelectedClass)) as SelectedClass;
+
+            if (component != null)
+            {
+                list.Add(component);
+            }
+        }
+        return list;
+    }
+}
---
+++ Assets/MainMonoBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMonoBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    GameObject character;
+    [SerializeField]
+    float speed;
+    
+    void Start(){
+        
+    }
+    
+    void Update(){
+        
+    }
+
+
+}
